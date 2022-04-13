9bd72f2b8e973f1baa77ecf71df5385d7a11c00b
NausheenKhanam <k05119921@gmail.com>
2022-04-13 17:19:59 +0530

    Added readme

--- src/test/java/SSinSelenium/screenshot.java
+++ src/test/java/SSinSelenium/screenshot.java
@@ -3,7 +3,11 @@ package SSinSelenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import library.Reusability;
 
 public class screenshot {
 	
@@ -13,13 +17,24 @@ public class screenshot {
 	public void ScreenshotsDemo()
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Documents\\DXC Selenium Automation Class\\ScreenshotsinSelenium\\BrowserJars\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.easycalculation.com/");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		driver.findElement(By.id("txtPassword123")).sendKeys("admin123");
 
 }
 	
+	@AfterMethod
 	
+	public void tearDown(ITestResult result)
+	{
+		if(ITestResult.FAILURE == result.getStatus() )
+		{
+			Reusability.capturedScreenShot(driver, result.getName());
+			
+		}
+		
+		   driver.quit();
+	}
 }
--- src/test/java/library/Reusability.java
+++ src/test/java/library/Reusability.java
@@ -1,5 +1,9 @@
 package library;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
@@ -8,9 +12,23 @@ public class Reusability {
 	public  static void capturedScreenShot(WebDriver driver, String screenShotName)
 	{
 		//Can't create instance for interface.We can Typecost
-		
+		try
+		{
 		TakesScreenshot ts = (TakesScreenshot)driver;
-		
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		 //Where you want to take screenshot
+		FileUtils.copyFile(source, new File ("./Screenshots/" + screenShotName + ".png"));
+	    System.out.println("Captured Screenshot by selenium webdriver");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Exception while taking screen shot"+ e.getMessage());
+		}
+	    
+	
+	
 	}
+	
+	
 }
 	
